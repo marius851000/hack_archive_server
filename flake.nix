d3c395865e996b0bcdf7f1e75dfec68737ee6ca6
marius david <mariusdavid@laposte.net>
2022-03-03 16:03:36 +0100

    Revert "get rid of naersk for memory reason"
    This reverts commit 0a9a3695a43a6b40f22885e88b983c70d6bea65c.

--- flake.nix
+++ flake.nix
@@ -1,22 +1,26 @@
 {
   inputs = {
     utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, naersk }:
     utils.lib.eachSystem (utils.lib.defaultSystems ++ ["armv7l-linux"]) (system: let
       pkgs = nixpkgs.legacyPackages."${system}";
+      naersk-lib = naersk.lib."${system}";
     in rec {
       # `nix build`
-      packages.pmd_hack_archive_server = pkgs.rustPlatform.buildRustPackage rec {
-        pname = "hacknews-server";
-        version = "0.1.0";
-
-        cargoSha256 = "sha256-7zNHqFgrUKYyNEROHAZPsnxgNF2tJJmcF+Ov9/4m4gM=";
-
-        src = ./.;
+      packages.pmd_hack_archive_server = naersk-lib.buildPackage {
+        pname = "pmd_hack_archive_server";
+        root = ./.;
       };
       defaultPackage = packages.pmd_hack_archive_server;
+
+      # `nix run`
+      apps.pmd_hack_archive_server = utils.lib.mkApp {
+        drv = packages.pmd_hack_archive_server;
+      };
+      defaultApp = apps.pmd_hack_archive_server;
 
       # `nix develop`
       devShell = pkgs.mkShell {
