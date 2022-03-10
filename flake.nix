d9b6c7bf5765b926ff06d4cb778835035c4b9183
marius david <mariusdavid@laposte.net>
2022-03-10 14:13:21 +0100

    update flake to crate2nix

--- flake.nix
+++ flake.nix
@@ -1,27 +1,26 @@
 {
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
+    crate2nix = {
+      url = "github:kolloch/crate2nix";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, utils, crate2nix }:
     utils.lib.eachSystem (utils.lib.defaultSystems ++ ["armv7l-linux"]) (system: let
       pkgs = nixpkgs.legacyPackages."${system}";
-      naersk-lib = naersk.lib."${system}";
+      crate2nix-tools = import "${crate2nix}/tools.nix" {inherit pkgs;};
+      generatedCargoNix = (crate2nix-tools.generatedCargoNix {
+        name = "pmd_hack_archive_server";
+        src = ./.;
+      });
+      importedCargoNix = import "${generatedCargoNix}/default.nix" { inherit pkgs; };
     in rec {
       # `nix build`
-      packages.pmd_hack_archive_server = naersk-lib.buildPackage {
-        pname = "pmd_hack_archive_server";
-        root = ./.;
-      };
+      packages.pmd_hack_archive_server = importedCargoNix.workspaceMembers.server.build;
       defaultPackage = packages.pmd_hack_archive_server;
-
-      # `nix run`
-      apps.pmd_hack_archive_server = utils.lib.mkApp {
-        drv = packages.pmd_hack_archive_server;
-      };
-      defaultApp = apps.pmd_hack_archive_server;
-
+      
       # `nix develop`
       devShell = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [ rustc cargo ];
