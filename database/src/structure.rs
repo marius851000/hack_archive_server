f2925df87f6fd0f39bdc4b6999fca64dce43699e
marius david <mariusdavid@laposte.net>
2022-03-11 13:54:56 +0100

    add simple change to test CI speed with cache

--- database/src/structure.rs
+++ database/src/structure.rs
@@ -1,12 +1,18 @@
 use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
+/// Token representing a major token.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MajorityCheck {
+    /// Who certified this user
     pub sources: Vec<ObjectId>,
+    /// If this require to have at least one source
     pub require_source: bool,
+    /// Arbitrary comment
     pub comment: String,
+    /// If it can certify another user
     pub can_perform_check: bool,
+    /// A secret token, owned by the major person
     pub token: String,
 }
 
